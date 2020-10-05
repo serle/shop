f3c504e3c6819d483a769f5d39d97baa8950c73b
serle <serle.shuman@gmail.com>
2020-10-05 21:57:44 +0200

    removed server hack

--- src/index.tsx
+++ src/index.tsx
@@ -4,9 +4,6 @@ import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 import ProductList from "./components/ProductList";
 
 
-//const server:string = process.env.NODE_ENV === 'development' ? 'localhost' : 'server'
-//console.log(`server: ${server}`);
-
 const client = new ApolloClient({
     uri: `http://localhost:3001/graphql`,
     cache: new InMemoryCache()
